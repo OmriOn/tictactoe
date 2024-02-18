8008444f4264c8f1d0bf9cbafd125879fe7acd00
Omri H <omri.hermon@gmail.com>
2024-02-18 19:06:29 +0200

    turn system

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 function setToX(x) {
     console.log(x);
     let y = document.getElementById(x);
-    y.textContent = 'X';
+    let turn = document.getElementById("playerIcon").textContent;
+    console.log(turn);
+    if (turn == 'X') {
+        y.textContent = 'X';
+        turn = 'O'
+        console.log("changed to X");
+    }
+    else {
+        y.textContent = 'O';
+        turn = 'X'
+        console.log("Changed to O");
+    }
+    playerIcon.textContent = turn;
 }
 
 function resetBoard() {
