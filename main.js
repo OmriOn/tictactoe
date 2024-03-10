b3dd0e051e4ff9c88c5bb30c6093306e671e2021
Omri H <omri.hermon@gmail.com>
2024-03-10 18:45:27 +0200

    win

--- main.js
+++ main.js
@@ -3,34 +3,23 @@ function setToX(x) {
     let y = document.getElementById(x);
     let turn = document.getElementById("playerIcon").textContent;
     console.log(turn);
-<<<<<<< HEAD
-    if (turn == 'X') {
-=======
     if (turn == 'X' && y.textContent == x) {
->>>>>>> f651530 (win)
         y.textContent = 'X';
         turn = 'O'
         console.log("changed to X");
     }
-<<<<<<< HEAD
     else {
-=======
-    else if (y.textContent == x) {
->>>>>>> f651530 (win)
         y.textContent = 'O';
         turn = 'X'
         console.log("Changed to O");
     }
     playerIcon.textContent = turn;
-<<<<<<< HEAD
-=======
     if (checkWin() == true) {
         {
             console.log("Win");
             resetBoard();
         }
     }
->>>>>>> f651530 (win)
 }
 
 function resetBoard() {
@@ -38,9 +27,6 @@ function resetBoard() {
         document.getElementById(i).innerHTML = i;
     }
     console.log("Board Reset");
-<<<<<<< HEAD
-}
-=======
 }
 function checkWin() {
     return check3Same(1, 2, 3) || check3Same(4, 5, 6) || check3Same(7, 8, 9) || check3Same(1, 5, 9) || check3Same(3, 5, 7) || check3Same(1, 4, 7) || check3Same(2, 5, 8) || check3Same(3, 6, 9)
@@ -51,4 +37,3 @@ function check3Same(a, b, c) {
     cellC = document.getElementById(c).textContent
     return cellA == cellB && cellB == cellC
 }
->>>>>>> f651530 (win)
